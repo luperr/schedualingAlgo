cc757843b909d139b8896618adb2509bd26f55fc
luper <lachlann_whitehill_93@hotmail.com>
2018-09-12 13:19:16 +1000

    added skeleton code for future class.  sett up using command line argument as the file to read from.  set out basic logic for the main function

--- main.c
+++ main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 
-int main(){
-    
-    printf("Hello Wold!\n");
+void fcfs(){
 
+}
+
+void srt(){
+
+}
+
+void rr2(){
+
+}
+
+void rr4(){
+
+}
+
+void avgWait(){
+
+}
+
+void avgTurnaround(){
+
+}
+
+int main(int argc, char *argv[]){
+    FILE *process = fopen(argv[0], "r");
+
+    
+    printf("FCFS\n");
+    printf("time\tPID\n");
+    //fcfs();
+    printf("SRT\n");
+    printf("time\tPID\n");
+    //srt();
+    printf("RR(2)\n");
+    printf("time\tPID\n");
+    //rr2();
+    printf("RR(4)\n");
+    printf("time\tPID\n");
+    //rr4();
+
+    fclose(process);
+    
     return 0;
 }
