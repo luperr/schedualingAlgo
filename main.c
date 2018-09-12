6f8d557672bcc9a43170715a9bed195f024431ff
luper <lachlann_whitehill_93@hotmail.com>
2018-09-12 17:44:18 +1000

    .

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 
+struct process{
+    int id;
+    int arrival;
+    int cycles;
+    int cyclesWaiting;
+    int turnaround;
+    // active?
+};
+
+//still not sold on this one
+struct output{
+    char algo[5];
+};
+
 void fcfs(){
 /*
  Takes in the number of process and the proccess info: then using
  the first come first serve method prints out each step of the process execution
- */
+
+ make a que of structs arraganged by lowest arrival time if 2 have the same arrival time lowest PID first 
+
+the execute in that order.
+while process[0].cycles > 0
+    process1.cycles -= 1
+
+
+print out:
+*/
 }
 
 void srt(){
@@ -14,17 +37,10 @@ do sortest remaining time maaaaaan
 */
 }
 
-void rr2(){
+void rr(){
 /*
-input: number of process, process info array
+input: number of process, process info array and a quanta 2 or 4
 round robin with a process time of 2
-*/
-}
-
-void rr4(){
-/*
-input: number of process, process info array
-round robin with a time for 4
 */
 }
 
@@ -41,7 +57,14 @@ void avgTurnaround(){
 }
 
 int main(int argc, char *argv[]){
-    FILE *process = fopen(argv[0], "r");
+    FILE *process = fopen(argv[1], "r");
+    //input argumejnts need to be handled
+    if(argc > 1){
+        printf("please only enter one comand line arguement");
+
+    }
+    ///turn each line of the file into an struct
+    //for all line except the first make a sruct
 
     //Probably change to a struct
     printf("FCFS\n");
